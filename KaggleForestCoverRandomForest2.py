7b09b69586125646ac45c2cffcee9d85410cb7fb
aspiringguru <bmatthewtaylor@gmail.com>
2016-10-24 17:34:37 +1000

    Kaggle forest Cover prediction added

---
+++ KaggleForestCoverRandomForest2.py
@@ -0,0 +1,63 @@
+"""
+https://www.kaggle.com/triskelion/forest-cover-type-prediction/first-try-with-random-forests/code
+https://archive.ics.uci.edu/ml/datasets/Covertype
+
+Todo : predict the forest cover type (the predominant kind of tree cover) from strictly cartographic variables
+(as opposed to remotely sensed data).
+
+
+
+"""
+
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+import numpy as np
+
+raw_data = "./covtype/covtype.data"
+
+df = pd.read_csv(raw_data, header=None)
+print "type(df)=", type(df)
+print "df.shape=", df.shape
+#print "df.head(5)=\n", df.head(5)
+
+#manually creating col_names since these are not in the .cvs file based on covtype.info
+col_names = ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology', \
+             'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm', \
+             'Horizontal_Distance_To_Fire_Points', 'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3', \
+             'Wilderness_Area4', 'ST1', 'ST2', 'ST3', 'ST4', 'ST5', 'ST6', 'ST7', 'ST8', 'ST9', 'ST10', 'ST11', \
+             'ST12', 'ST13', 'ST14', 'ST15', 'ST16', 'ST17', 'ST18', 'ST19', 'ST20', 'ST21', 'ST22', 'ST23', 'ST24', \
+             'ST25', 'ST26', 'ST27', 'ST28', 'ST29', 'ST30', 'ST31', 'ST32', 'ST33', 'ST34', 'ST35', 'ST36', 'ST37', \
+             'ST38', 'ST39', 'ST40', 'Cover_Type']
+
+print type(col_names), len(col_names)
+print "col_names", col_names
+#NB len(col_names)=55 & df.shape=(581012, 55), so # of columns matches.
+df.columns = col_names
+print "df.head(5)=\n", df.head(5)
+
+#now split dataframe into columns for features & keys
+keys = df['Cover_Type']
+print "type(keys)", type(keys)
+print "keys.shape=", keys.shape
+df = df.drop('Cover_Type', 1)
+print "df.shape=", df.shape
+print "list(df.columns)", list(df.columns)
+#print "df.head(5)=\n", df.head(5)
+
+np_features = df.as_matrix()
+np_keys = keys.as_matrix()
+
+features_train, features_test, keys_train, keys_test = train_test_split(np_features, np_keys, test_size=0.33, random_state=42)
+
+print "type(features_train)=", type(features_train), "features_train.shape=", features_train.shape
+print "type(features_test)=", type(features_test), "features_test.shape=", features_test.shape
+print "type(keys_train)=", type(keys_train), "keys_train.shape=", keys_train.shape
+print "type(keys_test)=", type(keys_test), "keys_test.shape=", keys_test.shape
+
+del df
+del np_features
+del np_keys
+
+clf = ensemble.RandomForestClassifier(n_estimators=200,n_jobs=-1,random_state=0)
+clf.fit(features_train, keys_train)
+
--- RandomForest1.py
+++ RandomForest1.py
@@ -1,6 +1,7 @@
 # Random Forest Classification
 import pandas
 import time
+import numpy as np
 from sklearn import cross_validation
 from sklearn.ensemble import RandomForestClassifier
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
@@ -14,24 +15,40 @@ Y = array[:,8]
 num_folds = 200
 num_instances = len(X)
 seed = 7
-num_trees = 100
+num_trees = 200
 max_features = 3
 #-------------- configure model end ----------------
 start_time = time.time()
 kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
 print("--- time to execute  cross_validation.KFold %s seconds ---" % (time.time() - start_time))
+#http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
+#K-Folds cross-validator
+# Provides train/test indices to split data in train/test sets.
+# Split dataset into k consecutive folds (without shuffling by default).
+# Each fold is then used once as a validation while the k - 1 remaining folds form the training set.
 
 start_time = time.time()
 model = RandomForestClassifier(n_estimators=num_trees, max_features=max_features)
 print("--- time to execute  RandomForestClassifier %s seconds ---" % (time.time() - start_time))
+#http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+# A random forest is a _meta estimator_ that fits a number of decision tree classifiers on various sub-samples
+# of the dataset and use averaging to improve the predictive accuracy and control over-fitting.
+# The sub-sample size is always the same as the original input sample size but the samples are drawn
+# with replacement if bootstrap=True (default).
+
+
 
 start_time = time.time()
 results = cross_validation.cross_val_score(model, X, Y, cv=kfold)
 print("--- time to execute  cross_validation.cross_val_score %s seconds ---" % (time.time() - start_time))
+#http://scikit-learn.org/stable/modules/cross_validation.html
 print "type(results)=", type(results)
 print "results.mean()=", results.mean()
 print "results.max()=", results.max()
 print "results.min()=", results.min()
+print "cross_validation.cross_val_score variances = ", np.var(results)
+print "cross_validation.cross_val_score standard deviation = ", np.std(results)
+print "mean +/- 1 std dev = ", results.mean() - np.std(results), ":", results.mean() + np.std(results)
 
 import matplotlib.pyplot as plt
 x = range(len(list(results)))
@@ -41,8 +58,8 @@ plt.scatter(x, y, alpha=0.5)
 plt.show()
 
 plt.xlabel('mean(cross_val_score)')
-plt.ylabel('Frequency')
-plt.title('Rando Forest for pima-indians-diabetes.data.')
+plt.ylabel('No of results')
+plt.title('Random Forest for pima-indians-diabetes.data.')
 plt.hist(y)
 plt.show()
 
